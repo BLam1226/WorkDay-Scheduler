5b1ae32ae85fc4ab6dec4910305a6a7e37ac90a9
BLam1226 <brendanlam0@gmail.com>
2023-07-03 16:23:48 -0400

    Added JavaScript and also minor addition to HTML to accomodate code

--- script.js
+++ script.js
@@ -1,23 +1,71 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+// Pulls current day from dayjs and writes it to paragraph
+$("#currentDay").text(dayjs().format("dddd, MMMM DD, YYYY"));
+
+// Pulls current hour in military time (24 hour) for hourColorSetter on intial page load before hourChecker initiates
+let currentHour = Number(dayjs().format("HH"));
+
+// Creates nodeList of all text area with class of description
+const taskInputs = $(".description");
+
+const hourColorSetter = ()=> {
+    // Iterates through nodeList of textareas with class of description
+    taskInputs.each(function() {
+        // If statement to compare the value of data-time in the textarea with current time to assign future/present/past
+
+        // For assigning future class
+        if ($(this).data("time") > currentHour) {
+            // Adds corresponding class to change color
+            $(this).parent().addClass("future");
+            // Removes any existing classes from previous day same with lines 26-27, 32-33
+            $(this).parent().removeClass("past");
+            $(this).parent().removeClass("present");
+
+        // For assigning present class
+        } else if ($(this).data("time") === currentHour) {
+            $(this).parent().addClass("present");
+            $(this).parent().removeClass("past");
+            $(this).parent().removeClass("future");
+
+        // For assigning past class
+        } else {
+            $(this).parent().addClass("past");
+            $(this).parent().removeClass("present");
+            $(this).parent().removeClass("future");
+        }
+    })
+}
+
+// Checks currentHour variable against pull from dayjs every second and updates if hour has chahnged
+const hourChecker = setInterval(() => {
+    // Pulls current day from dayjs and writes it to paragraph for updates
+    $("#currentDay").text(dayjs().format("dddd, MMMM DD, YYYY"));
+
+    // Sets currentHour equal to current hour pulled from dayjs
+    currentHour = Number(dayjs().format("HH"));
+
+    // Runs evaluation to update colors when hour changes
+    hourColorSetter();
+}, 1000);
+
+// Assigns click listener to saveBtns
+$(".saveBtn").on("click", (event) => {
+    // Pulls data-time attribute of sibling of textarea for associated save button
+    const taskHour = $(event.target).siblings(".description").data("time");
+
+    // Pulls value from textarea for associated save button
+    const task = $(event.target).siblings(".description").val();
+
+    // Pushes task value to local storage with data-time attribute as key for repopulating later
+    localStorage.setItem(taskHour, task);
 });
+
+// Called on runtime to iterate through nodeList of textareas and populate existing values from localStorage
+taskInputs.each(function() {
+    // Confirms item exists for specified hour in localStorage
+    if (localStorage.getItem($(this).data("time"))) {
+        // Populates textarea with value of matching key to data-time attribute
+        $(this).text(localStorage.getItem($(this).data("time")));
+    }
+})
+// Called on runtime to color timeblocks before hourChecker begins running
+hourColorSetter();
